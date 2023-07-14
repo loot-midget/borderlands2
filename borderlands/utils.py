6bf02d4083519fb8b0614f071a117e1a1a680d14
Loot Midget <loot-midget@users.noreply.github.com>
2023-07-15 00:54:30 +0300

    simplify

--- borderlands/utils.py
+++ borderlands/utils.py
@@ -6,6 +6,4 @@ MIN_PYTHON = (3, 9)
 def version_check():
     global MIN_PYTHON
     if sys.version_info < MIN_PYTHON:
-        sys.exit(
-            f'ERROR: Python {MIN_PYTHON[0]}.{MIN_PYTHON[1]} is required to run this utility'
-        )
+        sys.exit(f'ERROR: Python {MIN_PYTHON[0]}.{MIN_PYTHON[1]} is required to run this utility')
